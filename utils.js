94b7cd3265116a901811128bce14c42b973f60a1
zeerg9 <165725663+zeerg9@users.noreply.github.com>
2026-03-06 12:38:10 +0000

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:38:00.370Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-06T12:38:02.045Z
+Commit: Fix CSS styling bug on mobile devices
