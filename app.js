e99c6cb10908e4d604605c7af8529808ea84befc
zeerg9 <165725663+zeerg9@users.noreply.github.com>
2026-03-06 12:38:06 +0000

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:36:59.566Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T12:37:58.525Z
+Commit: Fix broken links in documentation
