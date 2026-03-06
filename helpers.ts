64b934b038c49d6bf9be96c54e89a3a6141313f4
zeerg9 <165725663+zeerg9@users.noreply.github.com>
2026-03-06 12:34:50 +0000

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:34:23.922Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-06T12:34:42.551Z
+Commit: Patch API endpoint error handling
