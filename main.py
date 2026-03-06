122e45d36e160ea947a6a0eb0f6f6b455423f740
zeerg9 <165725663+zeerg9@users.noreply.github.com>
2026-03-06 12:35:35 +0000

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:35:19.934Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-06T12:35:27.581Z
+Commit: Patch API endpoint error handling
