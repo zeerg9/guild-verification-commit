85a0199b1e0556e16054bc1157643b70872fde93
zeerg9 <165725663+zeerg9@users.noreply.github.com>
2026-03-06 12:36:27 +0000

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:36:04.038Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-06T12:36:19.368Z
+Commit: Fix race condition in async operations
